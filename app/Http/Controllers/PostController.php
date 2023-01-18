6fe98bb8ee016f78ba1adc20e1d7cbfb0eeabbd5
EligardoCruzM <elcruzma@ittepic.edu.mx>
2023-01-18 13:57:03 -0700

    modelo post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Support\Facades\DB;
 
 class PostController{
     public function index(){
-        $posts = DB::table('posts')->get();
+        $posts = Post::get();
     
         return view('blog', ['posts' => $posts]);
     }
